59184c42bc355692bf970d3b60121a59eb85fb5b
Yan Zhizhong <a2564011261@163.com>
2025-02-28 23:30:39 +0800

    add some things, such as login page, amazing page

--- app/src/main/java/top/mryan2005/hiandroid/MainActivity.java
+++ app/src/main/java/top/mryan2005/hiandroid/MainActivity.java
@@ -1,8 +1,13 @@
 package top.mryan2005.hiandroid;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -20,5 +25,30 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button loginButton = findViewById(R.id.button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // show a Dialog
+                LayoutInflater inflater = getLayoutInflater();
+                View dialogView = inflater.inflate(R.layout.activity_dialog_custom, null);
+
+                // Create and show the dialog
+                new AlertDialog.Builder(MainActivity.this)
+                        .setView(dialogView)
+                        .setPositiveButton("OK", (dialog, which) -> {
+                            // Handle OK button click
+                            Toast.makeText(MainActivity.this, "OK clicked", Toast.LENGTH_SHORT).show();
+                        })
+                        .setNegativeButton("Cancel", (dialog, which) -> {
+                            // Handle Cancel button click
+                            dialog.dismiss();
+                        })
+                        .show();
+            }
+        });
     }
+
 }
